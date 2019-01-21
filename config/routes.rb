c920474ace8f3db67a56b3cec14691ddb5a45531
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-20 22:11:01 -0200

    Adds except to edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :sales_platforms
   root 'webhooks#index'
 
   resources :webhooks
-  
+  resources :sales_platforms, except: :show
+
   namespace :api, constraints: { format: 'json' } do
     namespace :v1 do
       resources :webhooks
