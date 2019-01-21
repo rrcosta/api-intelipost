4a5cde7c92fbcc2b98a84131048dfb582ea5c44b
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-01-20 23:54:11 -0200

    Introduces Tracker Serializer

--- app/serializers/tracker_serializer.rb
+++ app/serializers/tracker_serializer.rb
@@ -1,8 +1,28 @@
 class TrackerSerializer < ActiveModel::Serializer
   attributes :id,
              :order_id,
-             event: { :status_id, :date },
-             package: { :package_id,
-                        package_invoice: { :number, :key, :date }
-                      }
+             event: :event_params,
+             package: :package_params
+
+  def event_params
+    {
+      :status_id,
+      :date
+    }
+  end
+
+  def package_params
+    {
+      :package_id,
+      package_invoice: package_invoice_params
+    }
+  end
+
+  def package_invoice_params
+    {
+      :number,
+      :key,
+      :date
+    }
+  end
 end
